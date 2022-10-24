e52da81770a63f2fd785abd9465ca683ff73db42
Petr Tran <petrblaztran@gmail.com>
2022-10-24 13:35:59 +0200

    Client initialization

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -1,18 +1,15 @@
-import React from 'react';
 import { Provider } from 'react-redux';
 import { Routes, Route } from 'react-router-dom';
-import { Layout, Login, Register } from '../pages/Pages';
+//import { Layout, Login, Register } from '../pages/Pages';
 import { store } from './store';
 
 const App = () => {
   return (
     <Provider store={store}>
-      <Layout>
         <Routes>
-          <Route path='login' element={<Login />} />
-          <Route path='register' element={<Register />} />
+          <Route path='login' />
+          <Route path='register' />
         </Routes>
-      </Layout>
     </Provider>
   );
 }
