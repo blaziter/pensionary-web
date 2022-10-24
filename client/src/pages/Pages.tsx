d1dbd3e64402974ec0252bf8964488f2cb044a7c
Petr Tran <petrblaztran@gmail.com>
2022-10-24 17:20:41 +0200

    TODO: Home Component, Admin Dashboard

--- client/src/pages/Pages.tsx
+++ client/src/pages/Pages.tsx
@@ -1,4 +1,5 @@
 export { default as Home } from '../components/home/Home.component';
+export { default as Admin } from '../components/admin/Admin.component';
 export { default as Login } from '../components/login/Login.component';
 export { default as Layout } from '../components/layout/Layout.component';
 export { default as Status } from '../components/status/StaffStatus.component';
