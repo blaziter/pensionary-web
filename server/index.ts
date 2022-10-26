95fa1122a1e68b9ff6fd0f00a32d4531569c8074
Stefan Jambrich <stefan.jambrich@student.spsmb.cz>
2022-10-26 22:34:21 +0200

    Minor changes, added cors

--- server/index.ts
+++ server/index.ts
@@ -1,5 +1,6 @@
 import express, { Express } from 'express';
 import dotenv from 'dotenv';
+const cors = require('cors');
 
 dotenv.config();
 
@@ -8,6 +9,11 @@ const employeesRouter = require('./routes/employee');
 
 const app: Express = express();
 const port = process.env.PORT;
+
+app.use(express.static('img'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors())
 
 app.use('/api', employeesRouter);
 
